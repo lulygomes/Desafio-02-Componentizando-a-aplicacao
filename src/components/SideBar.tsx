f1fb8398ee315fd7487bb8c29642615c9c7f6530
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-09-08 17:55:16 -0300

    update

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button'
+
+type GenresProps = {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+type SideBarProps = {
+  genres: GenresProps[]
+  selectedGenreId: Number
+  handleClickButton: Function
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: SideBarProps) {
+  
+  
+  return (
+  <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>)
 }
